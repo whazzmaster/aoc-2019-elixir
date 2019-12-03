0cef8554d2087da89886094b22f438a91d74905a
Zachery Moneypenny <zmoneypenny@cars.com>
2019-12-02 22:46:59 -0600

    Completes Day 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,36 @@
 defmodule AdventOfCode.Day02 do
   def part1(args) do
+    process(args)
   end
 
   def part2(args) do
   end
+
+  def process(input) do
+    instructions = Enum.chunk_every(input, 4, 4, [])
+    ins_idxs = Range.new(0, length(instructions) - 1) |> Enum.map(&(4 * &1))
+
+    Enum.reduce_while(ins_idxs, input, fn ins_idx, acc ->
+      ins = Enum.slice(acc, ins_idx, 4)
+      process_instruction(hd(ins), ins, acc)
+    end)
+  end
+
+  def process_instruction(1, ins, acc) do
+    [_, op_idx_1, op_idx_2, result_idx] = ins
+    op_1 = Enum.slice(acc, op_idx_1, 1) |> hd
+    op_2 = Enum.slice(acc, op_idx_2, 1) |> hd
+    result = op_1 + op_2
+    {:cont, List.replace_at(acc, result_idx, result)}
+  end
+
+  def process_instruction(2, ins, acc) do
+    [_, op_idx_1, op_idx_2, result_idx] = ins
+    op_1 = Enum.slice(acc, op_idx_1, 1) |> hd
+    op_2 = Enum.slice(acc, op_idx_2, 1) |> hd
+    result = op_1 * op_2
+    {:cont, List.replace_at(acc, result_idx, result)}
+  end
+
+  def process_instruction(99, _, acc), do: {:halt, acc}
 end
--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -3,19 +3,70 @@ defmodule AdventOfCode.Day02Test do
 
   import AdventOfCode.Day02
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
-
-    assert result
+  def get_inputs() do
+    input = File.read!(Path.join(__DIR__, "../../data/day2.txt"))
+    Enum.map(String.split(input, ","), &String.to_integer(&1))
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  test "sample-01" do
+    input = [1, 0, 0, 0, 99]
+    result = process(input)
+    assert result == [2, 0, 0, 0, 99]
+  end
 
-    assert result
+  test "sample-2" do
+    input = [2, 3, 0, 3, 99]
+    result = process(input)
+    assert result == [2, 3, 0, 6, 99]
+  end
+
+  test "sample-3" do
+    input = [2, 4, 4, 5, 99, 0]
+    result = process(input)
+    assert result == [2, 4, 4, 5, 99, 9801]
+  end
+
+  test "sample-4" do
+    input = [1, 1, 1, 4, 99, 5, 6, 0, 99]
+    result = process(input)
+    assert result == [30, 1, 1, 4, 2, 5, 6, 0, 99]
+  end
+
+  test "part1" do
+    input = get_inputs() |> List.replace_at(1, 12) |> List.replace_at(2, 2)
+    part1(input) |> IO.inspect(label: "Result")
+  end
+
+  test "part2" do
+    pristine_input = get_inputs()
+
+    nouns = Enum.to_list(0..99)
+    verbs = Enum.to_list(0..99)
+
+    [noun, verb] =
+      Enum.reduce_while(nouns, pristine_input, fn noun, input ->
+        nouned_input = List.replace_at(input, 1, noun)
+
+        case Enum.reduce_while(verbs, nouned_input, fn verb, input ->
+               nouned_and_verbed_input = List.replace_at(input, 2, verb)
+
+               #  IO.puts("Trying noun #{noun} and verb #{verb}")
+
+               case process(nouned_and_verbed_input) do
+                 [19_690_720, n, v | _] -> {:halt, [n, v]}
+                 _ -> {:cont, input}
+               end
+             end) do
+          [n, v] -> {:halt, [n, v]}
+          _ -> {:cont, input}
+        end
+      end)
+
+    IO.puts("Noun: #{Integer.to_string(noun)}")
+    IO.puts("Verb: #{Integer.to_string(verb)}")
+
+    result = 100 * noun + verb
+
+    IO.puts("Result: #{Integer.to_string(result)}")
   end
 end
