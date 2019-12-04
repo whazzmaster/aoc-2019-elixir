2357d4519c1214f55f36ea3c7710af3619d9b7a1
Zachery Moneypenny <zmoneypenny@cars.com>
2019-12-03 19:20:16 -0600

    Completes Day 3

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,81 @@
 defmodule AdventOfCode.Day03 do
-  def part1(args) do
+  # Part 1 Idea
+  # 0. Assume central port is at coords 0,0
+  # 1. Process the instructions to generate coordinate pairs at each step.
+  #    Result would look like: [[1,2], [3,4], [5,6]]
+  # 2. With two such structures, find the intersection. Those are where the wires cross.
+  # 3. Determine the manhattan distance between each intersection coord and 0,0
+  # 4. Return the shortest such distance
+  def part1(wires) do
+    intersections = find_intersections({0, 0}, wires)
+    # |> IO.inspect(label: "Intersections")
+
+    Enum.map(intersections, fn {x, y} -> {abs(x), abs(y)} end)
+    |> Enum.map(fn {x, y} -> x + y end)
+    |> Enum.min()
   end
 
-  def part2(args) do
+  def part2(wires) do
+    [route1, route2] = generate_routes({0, 0}, wires)
+    intersections = find_intersections({0, 0}, wires)
+
+    Enum.map(intersections, fn coord ->
+      route1_steps =
+        Enum.reduce_while(route1, 0, fn step, acc ->
+          if step == coord, do: {:halt, acc}, else: {:cont, acc + 1}
+        end)
+
+      route2_steps =
+        Enum.reduce_while(route2, 0, fn step, acc ->
+          if step == coord, do: {:halt, acc}, else: {:cont, acc + 1}
+        end)
+
+      route1_steps + route2_steps
+    end)
+    |> Enum.min()
+  end
+
+  def find_intersections(home, wires) do
+    [route1, route2] = generate_routes(home, wires)
+
+    # IO.puts("Length of wire1: #{length(route1)} steps")
+    # IO.puts("Length of wire2: #{length(route2)} steps")
+
+    route1_set = route1 |> Enum.into(MapSet.new())
+    route2_set = route2 |> Enum.into(MapSet.new())
+
+    MapSet.intersection(route1_set, route2_set)
+    |> MapSet.delete({0, 0})
+    |> Enum.to_list()
+  end
+
+  def generate_routes(home, wires) do
+    Enum.map(wires, fn wire -> generate_route(home, wire) end)
+  end
+
+  def generate_route(home, wire) do
+    # IO.puts("Generating route for wire with length #{length(wire)}")
+
+    {route, _} =
+      Enum.flat_map_reduce(wire, home, fn ins, {prev_x, prev_y} ->
+        # IO.puts(" - processing instruction #{ins}")
+
+        %{"direction" => direction, "distance" => distance} =
+          Regex.named_captures(~r/^(?<direction>U|D|L|R)(?<distance>\d+)/, ins)
+
+        distance = String.to_integer(distance)
+
+        steps =
+          case direction do
+            "U" -> Enum.map((prev_y + 1)..(prev_y + distance), &{prev_x, &1})
+            "D" -> Enum.map((prev_y - 1)..(prev_y - distance), &{prev_x, &1})
+            "R" -> Enum.map((prev_x + 1)..(prev_x + distance), &{&1, prev_y})
+            "L" -> Enum.map((prev_x - 1)..(prev_x - distance), &{&1, prev_y})
+          end
+
+        {steps, List.last(steps)}
+      end)
+
+    [home | route]
   end
 end
--- test/advent_of_code/day_03_test.exs
+++ test/advent_of_code/day_03_test.exs
@@ -3,18 +3,62 @@ defmodule AdventOfCode.Day03Test do
 
   import AdventOfCode.Day03
 
-  @tag :skip
+  def get_inputs() do
+    input = File.read!(Path.join(__DIR__, "../../data/day3.txt"))
+    lines = String.split(input, "\n")
+    Enum.map(lines, fn line -> String.split(line, ",") end)
+  end
+
+  test "generate_route/2 generates a route of coords" do
+    home = {0, 0}
+    wire = ["R8", "U5", "L5", "D3"]
+
+    route = generate_route(home, wire)
+
+    assert route == [
+             {0, 0},
+             {1, 0},
+             {2, 0},
+             {3, 0},
+             {4, 0},
+             {5, 0},
+             {6, 0},
+             {7, 0},
+             {8, 0},
+             {8, 1},
+             {8, 2},
+             {8, 3},
+             {8, 4},
+             {8, 5},
+             {7, 5},
+             {6, 5},
+             {5, 5},
+             {4, 5},
+             {3, 5},
+             {3, 4},
+             {3, 3},
+             {3, 2}
+           ]
+  end
+
+  test "find_intersections/2 finds intersecting points" do
+    home = {0, 0}
+    wires = [["R8", "U5", "L5", "D3"], ["U7", "R6", "D4", "L4"]]
+    intersections = find_intersections(home, wires)
+
+    assert intersections == [{3, 3}, {6, 5}]
+  end
+
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = get_inputs()
+    result = part1(input) |> IO.inspect(label: "Day 3, Part 1")
 
     assert result
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    input = get_inputs()
+    result = part2(input) |> IO.inspect(label: "Day 3, Part 2")
 
     assert result
   end
