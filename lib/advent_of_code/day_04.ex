5fbe1b5496b7d87bf4186e1c2ae945a839a03be7
Zachery Moneypenny <zmoneypenny@cars.com>
2019-12-04 16:13:04 -0600

    Completes Day 4

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,79 @@
 defmodule AdventOfCode.Day04 do
-  def part1(args) do
+  def part1(range) do
+    Enum.reduce(range, [], &test_password(&1, &2, part: :one))
+    |> length
   end
 
-  def part2(args) do
+  def part2(range) do
+    Enum.reduce(range, [], &test_password(&1, &2, part: :two))
+    |> length
+  end
+
+  def test_password(pw, compliant_passwords, part: part) do
+    initial = %{prev: nil, valid: false}
+
+    result =
+      Enum.reduce_while(Integer.digits(pw), initial, fn digit, acc ->
+        # IO.inspect(digit, label: "Processing digit")
+        # IO.inspect(acc, label: "Current acc")
+
+        case acc[:prev] do
+          nil ->
+            {:cont, %{prev: digit, dupes: %{}, halted: false}}
+
+          _ ->
+            cond do
+              # If the digit is greater than the previous one we can move on
+              acc[:prev] < digit ->
+                # IO.inspect("detected greater than")
+                updated = Map.merge(acc, %{prev: digit})
+                {:cont, updated}
+
+              # If the digit is less than the previous one then halt immediately
+              acc[:prev] > digit ->
+                # IO.inspect("detected less than, BAILING*********")
+                updated = Map.merge(acc, %{halted: true})
+                {:halt, updated}
+
+              # If the digit matches the previous one then update our dupes
+              # hash for later validation, depending on the conditions
+              acc[:prev] == digit ->
+                # IO.inspect("detected equals")
+                %{dupes: dupes} = acc
+
+                updated_dupes =
+                  if Map.has_key?(dupes, digit) do
+                    Map.merge(dupes, %{digit => dupes[digit] + 1})
+                  else
+                    Map.merge(dupes, %{digit => 2})
+                  end
+
+                updated = Map.merge(acc, %{prev: digit, dupes: updated_dupes})
+                {:cont, updated}
+            end
+        end
+      end)
+
+    # |> IO.inspect(label: "Result")
+
+    %{dupes: dupes} = result
+
+    # If we didn't halt and our dupes collection satisfies the current ruleset,
+    # then add the pw to the list of potential passwords
+    if !result[:halted] && satisfies_dupe_check(dupes, part) do
+      [pw | compliant_passwords]
+    else
+      compliant_passwords
+    end
+  end
+
+  def satisfies_dupe_check(dupes, :one) do
+    length(Map.keys(dupes)) > 0
+  end
+
+  def satisfies_dupe_check(dupes, :two) do
+    Enum.reduce_while(dupes, false, fn {_, occurrences}, _ ->
+      if occurrences == 2, do: {:halt, true}, else: {:cont, false}
+    end)
   end
 end
--- test/advent_of_code/day_04_test.exs
+++ test/advent_of_code/day_04_test.exs
@@ -3,19 +3,43 @@ defmodule AdventOfCode.Day04Test do
 
   import AdventOfCode.Day04
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  test "test_password/2 correctly tests a single password" do
+    input = 273_456
+    result = test_password(input, [], part: :one)
+    assert result == []
 
-    assert result
+    result = test_password(233_567, [], part: :one)
+    assert result == [233_567]
+
+    result = test_password(123_456, [], part: :one)
+    assert result == []
+
+    result = test_password(122_576, [], part: :one)
+    assert result == []
+
+    result = test_password(123_455, [], part: :one)
+    assert result == [123_455]
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  test "test_password/2 tests for additional part 2 requirements" do
+    result = test_password(111_122, [], part: :two)
+    assert result == [111_122]
 
-    assert result
+    result = test_password(123_444, [], part: :two)
+    assert result == []
+  end
+
+  test "part1" do
+    input = 273_025..767_253
+    result = part1(input) |> IO.inspect(label: "Day 4, Part 1")
+
+    assert result == 910
+  end
+
+  test "part2" do
+    input = 273_025..767_253
+    result = part2(input) |> IO.inspect(label: "Day 4, Part 2")
+
+    assert result == 598
   end
 end
